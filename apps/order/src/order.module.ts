3e1fe66d8663ec82ca3311d35f61a32adae42eba
Anawin Wiriyasathitkul <64160150@go.buu.ac.th>
2024-08-03 11:33:02 +0700

    finish

--- apps/order/src/order.module.ts
+++ apps/order/src/order.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [],
+  imports: [HttpModule],
   controllers: [OrderController],
   providers: [OrderService],
 })
-export class OrderModule {}
+export class OrderModule { }
